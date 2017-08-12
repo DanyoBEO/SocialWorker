6168a98479c2b8146e34a0bffc21244416ac1516
DanyoBEO <dan.bell.617@gmail.com>
2017-08-11 18:00:36 -0700

    Creating a database for forms submitted by users
    Creating an application where users will enter in patient information
    along with time staps, and send it to a google sheets page.

---
+++ src/SocialWork.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Daniel on 6/13/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SocialWork extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
